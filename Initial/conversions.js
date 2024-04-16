68241efe164e72df33ea0b14092efc33a7875a90
Tejaswini-Nandhu <125076323+Tejaswini-Nandhu@users.noreply.github.com>
2024-04-16 13:01:54 +0530

    Create conversions.js

---
+++ Initial/conversions.js
@@ -0,0 +1,11 @@
+// Results of Conversion of string, complex value, boolean.null,Undefined value into number data type
+// "33" => 33
+// "33aabc" => NaN : Not a number
+// true => 1; false => 0
+// null => 0
+//undefined => NaN
+
+//Results of Conversion of string, complex value, boolean.null,Undefined value into boolean data type
+// 20 => true
+let value=0;
+console.log(typeof(value))
