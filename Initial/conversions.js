cd1f43076114372edbba8c3d43d3a16e032f6dba
Tejaswini-Nandhu <125076323+Tejaswini-Nandhu@users.noreply.github.com>
2024-04-19 10:10:52 +0530

    Update conversions.js

--- Initial/conversions.js
+++ Initial/conversions.js
@@ -9,3 +9,22 @@
 // 20 => true
 let value=0;
 console.log(typeof(value))
+
+let undefinedvar;
+console.log(typeof undefinedvar)
+let bigNumber = 3453434546544557n
+console.log(typeof bigNumber);
+let testSymbol = Symbol(300)
+console.log([typeof testSymbol, testSymbol])
+let lakes=['ganga','yamuna','kaveri']
+console.log("Array",typeof lakes);
+let userDetails={
+    name:"Rajesh",
+    email:"rajesh@texas.com",
+    age:30,
+}
+console.log("Object",typeof userDetails);
+let myfunction= function(){
+    console.log("welcome to javascript")
+}
+console.log("Function",typeof myfunction);
