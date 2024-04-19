3e4bb6e4e032bcd838615104f728aa554c03f964
Tejaswini-Nandhu <125076323+Tejaswini-Nandhu@users.noreply.github.com>
2024-04-19 05:22:07 +0000

    Started learning Strings

---
+++ Initial/String.js
@@ -0,0 +1,9 @@
+//learning in detail about string
+
+//declarations of String
+
+let firstDeclaration = "Tejaswini"
+let secondDeclaration = new String("Tejaswini")
+/*`` these are now as backcaps which is the modern method to concatenate string. 
+Between these these symbols by using ${} we can include the data stored in the variables*/
+console.log(`We can declare string using these above two methods firstDeclaration which results ${firstDeclaration} and secondDeclarations which results ${secondDeclaration} . Both have same results`)
