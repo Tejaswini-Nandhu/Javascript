eef0e8984454f1154290dcb95de801e359506c6a
Tejaswini-Nandhu <125076323+Tejaswini-Nandhu@users.noreply.github.com>
2024-05-22 07:11:11 +0000

    number-practice

---
+++ Initial/numbers.js
@@ -0,0 +1,24 @@
+const my_num=400
+console.log(my_num);
+
+const sai_num=new Number(170)
+console.log(sai_num);
+
+console.log(sai_num.toExponential())
+
+const alt_num=sai_num.toString().length
+console.log(alt_num)
+
+var precision=29.024235
+console.log(precision.toPrecision(6));
+console.log(my_num.toFixed(1))
+
+const money = 1600000
+console.log(money.toLocaleString('en-IN'));
+
+//+++++++++++++++++MATH++++++++++++++++++++
+
+
+
+
+
