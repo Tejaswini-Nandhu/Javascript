5f2e809d219fd1c5953e74240b1e247018894afb
Tejaswini-Nandhu <125076323+Tejaswini-Nandhu@users.noreply.github.com>
2024-07-29 10:08:01 +0000

    day-2 ac1 task-1

---
+++ 30-day-JS-Challenge/DAY-2/activity1.js
@@ -0,0 +1,38 @@
+let number1 = 30;
+let number2 = 15;
+// Task 1 - write a program to add two numbers and log the result to the console
+
+function add(number1,number2){
+    return number1+number2;
+}
+
+let addition = add(number1, number2);
+console.log(addition);
+
+// Task 2 - write a program to subract two numbers and log the result to the console
+function sub(number1,number2){
+    return number1-number2;
+}
+let subraction = sub(number1, number2);
+console.log(subraction)
+
+// Task 3 - write a program to multiply two numbers and log the result to the console
+function multiply(number1,number2){
+    return number1*number2;
+}
+let multiplication = multiply(number1, number2);
+console.log(multiplication);
+
+// Task 1 - write a program to divide two numbers and log the result to the console
+function divide(number1,number2){
+    return number1/number2;
+}
+let division = divide(number1, number2);
+console.log(division);
+
+// Task 1 - write a program to find the remainder when one number is divided by another and log the result to the console
+function remainder(number1,number2){
+    return number1//number2;
+}
+let remainder_of_number = remainder(number1, number2);
+console.log(remainder_of_number);
